2e814e9605b4baa5ea9c48afca2d41fc300eb203
xAxee <50153178+xAxee@users.noreply.github.com>
2021-04-03 21:32:35 +0200

    idk

--- js/switch.js
+++ js/switch.js
@@ -2,24 +2,42 @@ let switchs = [];
 function registerSwitch(){
     document.querySelectorAll(".switch").forEach(function(item){
         let s = new Switch(item);
-        switchs.push(s)
+        if(item.onclick != null){
+            s.onClick = item.onclick;
+        }
+        switchs.push(s);
     })
     switchs.forEach(function(item){
         item.item.onclick = function(){
             item.toggle();
+            item.onClick();
         }
     })
-    switchs[0].onDisable = function(){
-        console.log("elo");
+
+    switchs[0].onClick = function(){
+        //todo
     }
 }
 
 class Switch{
-    constructor(item){
+    constructor(item, cookies=false){
         this.item = item;
+        if(item.id != null && item.id != ""){
+            this.id = item.id;
+            this.item.id = "switch_" + this.id;
+        } else {
+            this.id = null;
+        }
         this.status = !this.item.classList.contains("switch-disable");
         this.onDisable = function(){};
         this.onEnable = function(){};
+        this.onClick = function(){};
+        if(cookies){
+            
+        }
+    }
+    setValue(value){
+        
     }
     disable(){
         if(!this.item.classList.contains("switch-disable")){
