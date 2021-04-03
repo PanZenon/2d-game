3d8e085fa8cb338070bf16a40737245c6f31d627
PanZenon <karolwojtas2005@gmail.com>
2021-04-03 16:43:45 +0200

    game container

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 window.onload = ()=>{
     document.querySelectorAll(".toggleSettings").forEach(addToggle)
+    document.querySelectorAll(".toggleGame").forEach(addToggle)
 }
 
 function toggleSettings(){
     document.querySelector(".options").classList.toggle('disabled')
     document.querySelector(".menu").classList.toggle('disabled')
 }
+function toggleGame(){
+    document.querySelector(".game").classList.toggle('disabled')
+    document.querySelector(".menu").classList.toggle('disabled')
+}
 function addToggle(item){
-    item.onclick = toggleSettings
+    if(item.classList.contains("toggleGame")){
+        item.onclick = toggleGame
+    }
+    else{
+        item.onclick = toggleSettings
+    }
 }
